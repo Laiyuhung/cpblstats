dfb1b0c9de04bc36f453fc26b5b2516728f3e574
Laiyuhung <laiyuhung921118@gmail.com>
2025-05-26 23:53:58 +0800

    0526

--- src/app/gameRecord/[gameId]/page.js
+++ src/app/gameRecord/[gameId]/page.js
@@ -456,9 +456,6 @@ export default function GameRecord({ params }) {
     return (team.team_name === game.away && halfInning === 'top') || (team.team_name === game.home && halfInning === 'bottom');
   };
 
-  // 失誤全時段可編輯
-  const canEditError = () => true;
-
 
   if (isLoading) {
     return <div className="max-w-2xl mx-auto p-4 text-center">載入比賽資料中...</div>
