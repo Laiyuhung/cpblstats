6bf991ffa3b1ebd893d8daa216edc853ea8358b4
Laiyuhung <laiyuhung921118@gmail.com>
2025-05-31 23:13:21 +0800

    0531

--- src/app/gameRecord/[gameId]/page.js
+++ src/app/gameRecord/[gameId]/page.js
@@ -451,12 +451,6 @@ export default function GameRecord({ params }) {
   };
 
 
-  // 分數與失誤編輯權限：僅在目前半局可編輯分數，全時段可編輯失誤
-  const canEditScore = (team) => {
-    return (team.team_name === game.away && halfInning === 'top') || (team.team_name === game.home && halfInning === 'bottom');
-  };
-
-
   if (isLoading) {
     return <div className="max-w-2xl mx-auto p-4 text-center">載入比賽資料中...</div>
   }
